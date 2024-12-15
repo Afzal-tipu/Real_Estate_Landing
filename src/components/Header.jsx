6e346f2e9386ae19936b80e201ea351fbf885b5b
Afzal <afzal.tipu@gmail.com>
2024-12-15 17:37:16 +0600

    header modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
 
 
   return (
-    <div className='w-full sticky top-0 z-20 bg-white dark:bg-black'>
+    <div className="w-full sticky top-0 z-20 bg-white dark:bg-black">
       <header className="py-6 mb-12 border-b  ">
         <div className="container mx-auto flex justify-between items-center">
           {/* ====Logo===== */}
@@ -34,22 +34,22 @@ const Header = () => {
           {/* ====== Theme Button ===== */}
           <div></div>
 
-          <div className="flex items-center gap-6 justify-center">
+          <div className="flex items-center gap-4 lg:gap-8 justify-center">
             <button
-              className="bg-violet-700 px-2 py-2 rounded-full text-white"
+              className="bg-violet-500 px-1 py-1 xl:px-2 xl:py-2 text-lg rounded-full text-white"
               onClick={handleThemeSwitch}
             >
               {theme === "light" ? <RiMoonFill /> : <RiSunFill />}
             </button>
 
             <Link
-              className=" hover:text-violet-900 hover:scale-105 font-semibold dark:text-white"
+              className=" hover:text-violet-900 hover:scale-105 text-sm lg:text-lg font-semibold dark:text-white"
               to=""
             >
               Login
             </Link>
             <Link
-              className="bg-violet-600 hover:bg-violet-700 hover:scale-105 transition text-white py-2 px-4 border rounded-lg font-semibold"
+              className="bg-violet-600 hover:bg-violet-700 hover:scale-105 transition text-white text-sm lg:text-lg py-2 px-2 xl:px-4 border rounded-lg font-semibold"
               to=""
             >
               Sign Up
